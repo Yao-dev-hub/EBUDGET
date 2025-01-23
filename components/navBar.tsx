6b6bb2f9b3b88b827b2cee3e3e17a8496d3ee8b7
yao <parfaitericyao123@gmail.com>
2025-01-23 18:34:53 +0000

    7

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -1,22 +1,25 @@
 "use client"
 import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown'
 import Dropdown from 'react-bootstrap/Dropdown'
-import { FaCircleUser } from "react-icons/fa6";
 import { UsersType } from '@/Type';
+import { useEffect, useState } from 'react';
 
 function NavBar() {
 
-    const path = document.location.pathname
-    const user: UsersType = JSON.parse(localStorage.getItem("userInfo")!)
-    if (!user) {
-        document.location.href = "/clients/users-pages/login"
-        return;
-    }
+    const [path, setPath] = useState("")
+    const [user, setUser] = useState<UsersType>({})
+
+    useEffect(() => {
+        setTimeout(() => {
+            const userInfo = localStorage.getItem("userInfo")!
+            setPath(window.location.pathname)
+            setUser(JSON.parse(userInfo))
+        }, 100)
+    }, [])
 
     if (path !== "/clients/users-pages/sign-up" && path !== "/clients/users-pages/login") {
 
@@ -28,16 +31,16 @@ function NavBar() {
                         <Navbar.Toggle aria-controls="basic-navbar-nav" />
                         <Navbar.Collapse id="basic-navbar-nav mb-lg-0">
                             <Nav className="mx-auto">
-                                <Nav.Link href="/clients/dashbord/dash" className="btn btn-warning fw-bold btn-sm ">Tableau de bord</Nav.Link>
-                                <Nav.Link href="/clients/dashbord/budgets/new-budget" className=" btn btn-warning fw-bold btn-sm left-margin mx-2">Mes Budgets</Nav.Link>
-                                <Nav.Link href="/clients/dashbord/transactions" className=" btn btn-warning fw-bold btn-sm">Mes transactions</Nav.Link>
+                                <Link href="/clients/dashbord/dash" className="btn btn-warning fw-bold btn-sm ">Tableau de bord</Link>
+                                <Link href="/clients/dashbord/budgets/new-budget" className=" btn btn-warning fw-bold btn-sm left-margin mx-2">Mes Budgets</Link>
+                                <Link href="/clients/dashbord/transactions" className=" btn btn-warning fw-bold btn-sm">Mes transactions</Link>
                             </Nav>
 
                             <Dropdown className='d-flex justify-content-center justify-content-md-start'>
                                 {
                                     user && user !== undefined && (
                                         <div>
-                                            <img src={user.photo !== "" ? user.photo : "/img/user.png"} alt="user" style={{ width: "30px", height: "30px", borderRadius: "50%" }} />
+                                            {/* <img src={user.photo !== "" ? user.photo : "/img/user.png"} alt="user" style={{ width: "30px", height: "30px", borderRadius: "50%" }} /> */}
                                             <NavDropdown title={user.nom} id="basic-nav-dropdown">
                                                 <NavDropdown.Item href="#action/3.1">Se deconnecter</NavDropdown.Item>
                                             </NavDropdown>
