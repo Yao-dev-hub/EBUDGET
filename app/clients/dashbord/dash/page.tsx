cfade3134060f81923728fa98488ae1e0b234d83
yao <parfaitericyao123@gmail.com>
2025-01-23 17:32:35 +0000

    6

--- app/clients/dashbord/dash/page.tsx
+++ app/clients/dashbord/dash/page.tsx
@@ -1,21 +1,9 @@
-import React from 'react'
-import InfosTransactions from './InfosTransactions'
 
 function page() {
     return (
         <>
             <main className='container infos-transaction mt-2'>
-                <div className="row">
-                    <div className="col-md-12 ">
-                        <InfosTransactions />
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col-md-8">
-
-                    </div>
-                    <div className="col-md-4"></div>
-                </div>
+                <p>En construction</p>
             </main>
         </>
     )
